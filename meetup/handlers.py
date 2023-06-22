fedcc30abe1eed921cb1f80e4f0d76cc28624419
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-06-23 01:58:15 +0300

    add some menus

--- meetup/handlers.py
+++ meetup/handlers.py
@@ -1,9 +1,12 @@
 from config import BOT_TOKEN
-from keyboards import role_selection_keyboard
+from keyboards import (role_selection_keyboard, get_id_keyboard, next_keyboard,
+                       kb_builder)
 from texts import TEXTS
 
+from temporary_data import speakers, reports
+
 from aiogram import Bot, Router
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
 from aiogram.filters import CommandStart, Text, StateFilter
 from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -33,7 +36,48 @@ async def process_start_command(message: Message, state: FSMContext):
     await state.set_state(default_state)
 
 
-@router.message(StateFilter(default_state), Text(text='Спикер'))
+# ветка спикера
+# @router.message(StateFilter(default_state), Text(text='Спикер'))
+@router.message(Text(text='Спикер'))
 async def speaker_greeting(message: Message, state: FSMContext):
-    await message.answer(text=TEXTS['speaker_greeting'])
-    await state.set_state(FSM.speaker_state)
+    # TODO запрос в БД с данными о спикерах
+    if message.from_user.id in speakers:
+        await message.answer(text=TEXTS['speaker_greeting'].format(message.from_user.first_name),  # TODO здесь необходимо подтянуть имя спикера
+                             reply_markup=next_keyboard)
+        # await state.set_state(FSM.speaker_state)
+    else:
+        await message.answer(text=TEXTS['speaker_not_recognized'],
+                             reply_markup=get_id_keyboard)
+
+
+# @router.callback_query(StateFilter(default_state), Text(text='get_id'))
+@router.callback_query(Text(text='get_id'))
+async def get_id(callback: CallbackQuery):
+    await callback.message.answer(
+        text=f'Ваш telegram id:\n{callback.from_user.id}')
+
+
+# @router.callback_query(StateFilter(FSM.speaker_state), Text(text='next'))
+@router.callback_query(Text(text='next'))
+async def display_reports(callback: CallbackQuery):
+    text = 'Выберите доклад из списка запланированных мероприятий, чтобы начать доклад или прочитать вопросы по докладу:\n'
+    # запрос в БД для получения списка докладов
+    for report in reports:
+        text += TEXTS['reports'].format(reports.index(report) + 1, report['Тема доклада'], report['Дата и время'], report['Место проведения'])
+    buttons = [InlineKeyboardButton(text=report['Тема доклада'], callback_data=report['Тема доклада']) for report in reports]
+    keyboard = kb_builder.row(*buttons, width=1)
+    # await callback.answer()
+    await callback.message.answer(text=text, reply_markup=keyboard.as_markup())
+
+
+# print(callback.json(indent=4, exclude_none=True))
+# ветка гостя
+# @router.message(StateFilter(default_state, FSM.speaker_state), Text(text='Гость мероприятия'))
+@router.message(Text(text='Гость мероприятия'))
+async def guest_greeting(message: Message, state: FSMContext):
+    if message.from_user.id in speakers:
+        await message.answer(text=TEXTS['guest_greeting'].format(message.from_user.first_name))
+        # await state.set_state(FSM.guest_state)
+
+
+# print(callback.json(indent=4, exclude_none=True))
--- meetup/keyboards.py
+++ meetup/keyboards.py
@@ -1,9 +1,22 @@
 from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
                            InlineKeyboardButton, InlineKeyboardMarkup)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 speaker_button = KeyboardButton(text='Спикер')
 guest_button = KeyboardButton(text='Гость мероприятия')
 
+get_id_button = InlineKeyboardButton(text='Узнать свой telegram id',
+                                     callback_data='get_id')
+next_button = InlineKeyboardButton(text='Далее',
+                                   callback_data='next')
+
+
 role_selection_keyboard = ReplyKeyboardMarkup(
-    keyboard=[[speaker_button], [guest_button]], resize_keyboard=True)
+    keyboard=[[speaker_button], [guest_button]],
+    resize_keyboard=True,
+    one_time_keyboard=True)
+
+kb_builder = InlineKeyboardBuilder()
+get_id_keyboard = InlineKeyboardMarkup(inline_keyboard=[[get_id_button]])
+next_keyboard = InlineKeyboardMarkup(inline_keyboard=[[next_button]])
