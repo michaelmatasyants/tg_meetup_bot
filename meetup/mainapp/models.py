2b0b27b03ad6f0a07067d45ca946ac0d53e99143
Michael Matasyants <michael.matasyanc@gmail.com>
2023-06-22 15:26:09 +0300

    combine User and Speaker classes

--- meetup/mainapp/models.py
+++ meetup/mainapp/models.py
@@ -2,23 +2,32 @@ from django.db import models
 from django.core import validators
 
 
+USER_ROLE_CHOICES = [
+    ("L", "Listener"),
+    ("S", "Speaker"),
+    ("O", "Organizer"),
+]
+
 class Event(models.Model):
     date = models.DateField()
     start_time = models.TimeField()
     event_name = models.CharField(max_length=50)
     place = models.CharField(max_length=50)
-    is_over = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f'{self.event_name} {self.date}'
 
 
-class Speaker(models.Model):
+class User(models.Model):
     tg_id = models.CharField(max_length=30, unique=True)
-    full_name = models.CharField(max_length=50)
-    workplace = models.CharField(max_length=50)
-    experience = models.TextField()
-    events = models.ManyToManyField(Event)
+    email = models.EmailField(
+        max_length=254, blank=True,
+        validators=[validators.EmailValidator(message="Invalid Email")])
+    full_name = models.CharField(max_length=50, blank=True)
+    workplace = models.CharField(max_length=150, blank=True)
+    experience = models.TextField(blank=True)
+    role = models.CharField(max_length=20, choices=USER_ROLE_CHOICES,
+                            default='L')
 
     def __str__(self) -> str:
         return f'{self.full_name}'
@@ -31,7 +40,7 @@ class Report(models.Model):
     actual_start_time = models.TimeField(null=True)
     actual_end_time = models.TimeField(null=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    speaker = models.OneToOneField(Speaker, on_delete=models.CASCADE)
+    speaker = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f'{self.report_title}'
@@ -42,20 +51,11 @@ class Report(models.Model):
         return False
 
 
-class User(models.Model):
-    email = models.EmailField(
-        max_length=254, null=True,
-        validators=[validators.EmailValidator(message="Invalid Email")])
-    tg_id = models.CharField(max_length=30, unique=True)
-
-    def __str__(self) -> str:
-        return f"User's telegram id: {self.tg_id}"
-
-
 class Question(models.Model):
+    question_title = models.CharField(max_length=150)
     question_text = models.TextField(unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    speaker = models.OneToOneField(Speaker, on_delete=models.CASCADE)
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.question_text}'
+        return f'{self.question_title}'
